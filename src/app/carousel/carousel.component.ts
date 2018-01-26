4b8a687677fb13b245bef902f6f4a57c0ecd9de3
Kathy <kathyy.ma@gmail.com>
2018-01-26 13:32:16 -0800

    Updated carousel styles.

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -5,7 +5,7 @@ import { SamplesService } from '../services/samples.service';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
   @Input() show: boolean;
