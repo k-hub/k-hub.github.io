59ae6741dd3d59038447dfaeac808ea2ff22e8ac
Kathy <kathyy.ma@gmail.com>
2018-02-02 13:23:17 -0800

    Added poster images for vids.

--- src/app/samples.ts
+++ src/app/samples.ts
@@ -2,17 +2,20 @@ export const SAMPLES: Array<object> = [
 	{ name: 'guestSendWidget',
 	  src: '../assets/sample-work/guest_send_widget.mp4',
 	  type: 'video',
-	  title: 'Guest Send Widget'
+	  title: 'Guest Send Widget',
+	  poster: '../assets/sample-work/guest_send_widget_poster.png'
 	},
 	{ name: 'permissions',
 	  src: '../assets/sample-work/permissions.mp4',
 	  type: 'video',
-	  title: 'Setting permissions for a Space'
+	  title: 'Setting permissions for a Space',
+	  poster: '../assets/sample-work/permissions_poster.png'
 	},
 	{ name: 'approvals',
 	  src: '../assets/sample-work/approvals.mov',
 	  type: 'video',
-	  title: 'Creating an Approval Request'
+	  title: 'Creating an Approval Request',
+	  poster: '../assets/sample-work/approvals_poster.png'
 	},
 	{ name: 'editApproval',
 	  src: '../assets/sample-work/edit_approval.png',
